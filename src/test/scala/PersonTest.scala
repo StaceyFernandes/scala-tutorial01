9dff3553e11edf2fe349a1f81133897b9f8c6d6d
Stacey Fernandes <stacey.fernandes@rea-group.com>
2019-05-18 19:07:46 +1000

    Fix formatting

--- src/test/scala/PersonTest.scala
+++ src/test/scala/PersonTest.scala
@@ -5,10 +5,10 @@ import org.specs2.mutable.Specification
 class PersonTest extends Specification {
 
   "getAge" should {
-    "return age" in  {
-      val child = new Person("jil",  LocalDateTime.of(2015, Month.JANUARY, 25,6,30))
-      child.getAge( LocalDateTime.of(2019,Month.JANUARY, 25,6,30)) must equalTo(4)
+    "return age" in {
+      val child = new Person("jil", LocalDateTime.of(2015, Month.JANUARY, 25, 6, 30))
+      child.getAge(LocalDateTime.of(2019, Month.JANUARY, 25, 6, 30)) must equalTo(4)
     }
-    }
+  }
 
 }
